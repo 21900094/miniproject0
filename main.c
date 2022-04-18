fff0973a3cf9f34331d192a938ac1e79b0a25104
kimminsang <21900094@handong.ac.kr>
2022-04-18 12:57:26 +0900

    ninth commit

--- main.c
+++ main.c
@@ -10,7 +10,7 @@ int main(void){
     int now=0;
     now=load(p);
     int result;
-    printf("commit number 8\n");
+    printf("commit number 9\n");
     while (1){
         menu = selectMenu();
         if (menu == 0) {
